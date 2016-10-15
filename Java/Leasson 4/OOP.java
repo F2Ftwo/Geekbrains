ab66ad39d956b75c3deddf3eeeaff15fafa82d93
Ftwo <yan.smirnov18@gmail.com>
2016-10-15 13:41:00 +0400

    Test Class - Тестируем Кота

---
+++ Java/Leasson 4/OOP.java
@@ -0,0 +1,13 @@
+class OOP {
+	public static void main(String args[]) {
+		Cat c1 = new Cat("Murzik", "Black", 6);
+		Cat c2 = new Cat("Vasil", "Red", 3);
+		Cat c3 = new Cat("Gosha", "Black and White", 5);
+		c1.Draw();
+		c1.Say();
+		c2.Draw();
+		c2.Say();
+		c3.Draw();
+		c3.Say();
+		}
+}
